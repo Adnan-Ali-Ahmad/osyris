7930b5ecafcf517f0252816a14547aef5c2d1558
Adnan-Ali-Ahmad <adnan.ali.ahmad1998@gmail.com>
2024-02-16 11:56:14 +0100

    debug

--- src/osyris/core/array.py
+++ src/osyris/core/array.py
@@ -142,7 +142,7 @@ class Array(Base):
         return np.reciprocal(self / other)
 
     def __pow__(self, number):
-        return np.power(self, number.values)
+        return np.power(self.values, number)
 
     def __neg__(self):
         return np.negative(self)
