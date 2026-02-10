3e08462dff7cc0940f7f0a0b9bbdc08bc79f4094
Adnan Ali Ahmad <aaahmad@adnanmbp.cral.ens-lyon.fr>
2026-02-10 11:38:07 +0100

    project coords to new basis rather than performing a rotation

--- src/osyris/spatial/coordinate_transforms.py
+++ src/osyris/spatial/coordinate_transforms.py
@@ -50,3 +50,42 @@ def rotate(dataset, new_basis, dr_L=None):
                     z=vector[2],
                     unit=dataset[g][element].unit)
     dataset.basis = new_basis
+
+def rotate(dataset, new_basis, dr_L=None):
+    """
+    Rotates all vectors in dataset to align with vector in 'new_basis'
+    """
+    new_basis = utils._parse_basis(dataset, new_basis, dr_L)
+    
+    new_z = np.array([new_basis.x.values, new_basis.y.values, new_basis.z.values])
+    new_z = new_z / np.linalg.norm(new_z)
+
+    # if new_basis is co-linear with old_basis
+    if abs(new_z[2]) < 0.99:
+        new_x = np.cross([0, 0, 1], new_z)
+    else:
+        new_x = np.cross([0, 1, 0], new_z)
+    new_x = new_x / np.linalg.norm(new_x)
+
+    new_y = np.cross(new_z, new_x)
+
+    for g in dataset.groups.keys():
+        for element in dataset[g]:
+            vec_obj = dataset[g][element]
+            if hasattr(vec_obj, 'nvec') and vec_obj.nvec == 3:
+                x = vec_obj.x.values
+                y = vec_obj.y.values
+                z = vec_obj.z.values
+
+                xx = x * new_x[0] + y * new_x[1] + z * new_x[2]
+                yy = x * new_y[0] + y * new_y[1] + z * new_y[2]
+                zz = x * new_z[0] + y * new_z[1] + z * new_z[2]
+
+                dataset[g][element] = vec_obj.__class__(
+                    x=xx,
+                    y=yy,
+                    z=zz,
+                    unit=vec_obj.unit
+                )
+
+    dataset.basis = new_z
