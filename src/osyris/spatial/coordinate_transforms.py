0431e152ccdf519ef1f34eb60081c74112bd422c
Adnan Ali Ahmad <aaahmad@adnanmbp.cral.ens-lyon.fr>
2026-02-10 13:46:47 +0100

    forgot to comment old rotate function

--- src/osyris/spatial/coordinate_transforms.py
+++ src/osyris/spatial/coordinate_transforms.py
@@ -18,38 +18,38 @@ def translate(dataset, new_origin):
     dataset.origin = new_origin
 
 
-def rotate(dataset, new_basis, dr_L=None):
-    """
-    Rotates all vectors in dataset to align with vector in 'new_basis'
-    """
-    new_basis = utils._parse_basis(dataset, new_basis, dr_L)
+# def rotate(dataset, new_basis, dr_L=None):
+#     """
+#     Rotates all vectors in dataset to align with vector in 'new_basis'
+#     """
+#     new_basis = utils._parse_basis(dataset, new_basis, dr_L)
 
-    try:
-        old_basis = dataset.basis
-    except AttributeError:
-        old_basis = [0, 0, 1]  # assume it's the ramses grid
-    if hasattr(new_basis, 'nvec'):  # if it's a vector
-        new_basis = [new_basis.x.values, new_basis.y.values, new_basis.z.values]
-    rot_angle = np.arccos(
-        np.dot(old_basis, new_basis) /
-        (np.linalg.norm(old_basis) * np.linalg.norm(new_basis)))
-    rot_vector = np.cross(new_basis, old_basis)
-    r_m = utils._rotation_matrix(rot_vector, rot_angle)
-    for g in dataset.groups.keys():
-        for element in dataset[g]:
-            if hasattr(dataset[g][element], 'nvec'):
-                # all of this will be simplified once matmul is integraded into Vector
-                vector = np.array([
-                    dataset[g][element].x.values, dataset[g][element].y.values,
-                    dataset[g][element].z.values
-                ])
-                vector = r_m @ vector
-                dataset[g][element] = dataset[g][element].__class__(
-                    x=vector[0],
-                    y=vector[1],
-                    z=vector[2],
-                    unit=dataset[g][element].unit)
-    dataset.basis = new_basis
+#     try:
+#         old_basis = dataset.basis
+#     except AttributeError:
+#         old_basis = [0, 0, 1]  # assume it's the ramses grid
+#     if hasattr(new_basis, 'nvec'):  # if it's a vector
+#         new_basis = [new_basis.x.values, new_basis.y.values, new_basis.z.values]
+#     rot_angle = np.arccos(
+#         np.dot(old_basis, new_basis) /
+#         (np.linalg.norm(old_basis) * np.linalg.norm(new_basis)))
+#     rot_vector = np.cross(new_basis, old_basis)
+#     r_m = utils._rotation_matrix(rot_vector, rot_angle)
+#     for g in dataset.groups.keys():
+#         for element in dataset[g]:
+#             if hasattr(dataset[g][element], 'nvec'):
+#                 # all of this will be simplified once matmul is integraded into Vector
+#                 vector = np.array([
+#                     dataset[g][element].x.values, dataset[g][element].y.values,
+#                     dataset[g][element].z.values
+#                 ])
+#                 vector = r_m @ vector
+#                 dataset[g][element] = dataset[g][element].__class__(
+#                     x=vector[0],
+#                     y=vector[1],
+#                     z=vector[2],
+#                     unit=dataset[g][element].unit)
+#     dataset.basis = new_basis
 
 def rotate(dataset, new_basis, dr_L=None):
     """
